02677cb26926f731778af29f15ececb43e354545
Alona <alona.sorochynska@gmail.com>
2024-05-29 19:18:10 +0100

    create models for airport

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,50 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255)
+
+
+class Route(models.Model):
+    distance = models.IntegerField()
+    source = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="source_routes")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="destination_routes")
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE, related_name="airplanes")
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Flight(models.Model):
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name="route_flights")
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE, related_name="airplane_flights")
+    crew = models.ManyToManyField(Crew, related_name="crew_flights")
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name="flight_tickets")
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_tickets")
+
