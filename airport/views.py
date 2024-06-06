d6f421837915d3a37fc5c07879cb02ea69706e23
Alona <alona.sorochynska@gmail.com>
2024-06-06 19:26:40 +0100

    define get_serializer_class for AirplaneViewSet

--- airport/views.py
+++ airport/views.py
@@ -4,47 +4,52 @@ from airport.models import (
     Airport, AirplaneType, Crew, Flight, Order, Ticket, Airplane, Route
 )
 from airport.serializers import (
-    airport_serializer, airplane_serializers, crew_serializers,
-    flight_serializers, order_serializers, route_serializers,
-    ticket_serializers
-)
+    AirportSerializer, RouteSerializer, AirplaneTypeSerializer, AirplaneSerializer, AirplaneListSerializer,
+    AirplaneDetailSerializer, CrewSerializer, FlightSerializer, OrderSerializer, TicketSerializer)
 
 
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
-    serializer_class = airport_serializer.AirportSerializer
+    serializer_class = AirportSerializer
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
-    serializer_class = route_serializers.RouteSerializer
+    serializer_class = RouteSerializer
 
 
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
-    serializer_class = airplane_serializers.AirplaneTypeSerializer
+    serializer_class = AirplaneTypeSerializer
 
 
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.all()
-    serializer_class = airplane_serializers.AirplaneSerializer
+    serializer_class = AirplaneSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AirplaneListSerializer
+        if self.action == "retrieve":
+            return AirplaneDetailSerializer
+        return self.serializer_class
 
 
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
-    serializer_class = crew_serializers.CrewSerializer
+    serializer_class = CrewSerializer
 
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
-    serializer_class = flight_serializers.FlightSerializer
+    serializer_class = FlightSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
-    serializer_class = order_serializers.OrderSerializer
+    serializer_class = OrderSerializer
 
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
-    serializer_class = ticket_serializers.TicketSerializer
+    serializer_class = TicketSerializer
